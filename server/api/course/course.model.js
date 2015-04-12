7f8f598c28bef1b41ac1f04184e7e418175b8557
Gareth Lacey <gareth_1992@live.com>
2015-04-12 21:46:39 +0100

    added mongoose validation and course rating based on user comments

--- server/api/course/course.model.js
+++ server/api/course/course.model.js
@@ -3,6 +3,23 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+var youtubeCheck = [
+function(v){
+  var test = v.trim()
+  if(v.indexOf("youtube") == -1){
+    return false
+  }
+},
+'The value entered is not a valid {PATH} link'
+]
+
+var emptyCheck = [
+function(v){
+  return (v.trim().length > 0)
+},
+'{PATH} cannot be an empty variable!'
+]
+
 var ReplySchema = new Schema({
   UID: {type: String, required: true},
   email: {type: String, required: true},
@@ -20,7 +37,7 @@ var CommentSchema = new Schema({
   email: {type: String, required: true, match: /@/},
 	name: {type:String, required: true},
   rate: {type:Number, required: true, min: 1, max: 5, default: 1},
-	post: {type:String, required: true},
+	post: {type:String, required: true, validate: emptyCheck},
 	created_at: {type:String, required: true},
 	course_id: {type:String, required: true},
   replies: [ReplySchema]
@@ -29,11 +46,11 @@ var CommentSchema = new Schema({
 
 
 var CourseSchema = new Schema({
-  title: {type:String, required: true},
+  title: {type:String, required: true, validate: emptyCheck},
   rate: {type:Number, required: true, min: 0, max: 5, default: 0},
-  language: {type:String, required: true},
-  description: {type:String, required: true},
-  youtube: {type:String, required: true, match: /youtube/},
+  language: {type:String, required: true, validate: emptyCheck},
+  description: {type:String, required: true, validate: emptyCheck},
+  youtube: {type:String, required: true, validate: youtubeCheck},
   difficulty: {
     type:String,
     enum: ['Beginner', 'Intermediate', 'Advanced'], 
@@ -41,6 +58,7 @@ var CourseSchema = new Schema({
   views: {type:Number, required: true, min: 0, default: 0},
   comments: [CommentSchema]
 });
+
 
 
 
